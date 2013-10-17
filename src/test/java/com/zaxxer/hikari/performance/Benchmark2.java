76c9f95573de20744b597e8162d9b40784a73f67
Brett Wooldridge <brett.wooldridge@gmail.com>
2013-10-17 19:44:27 +0900

    Remove unused import

--- src/test/java/com/zaxxer/hikari/performance/Benchmark2.java
+++ src/test/java/com/zaxxer/hikari/performance/Benchmark2.java
@@ -18,7 +18,6 @@ package com.zaxxer.hikari.performance;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
