199cd2b835ef517cefa4f0074aa72e69087b5e1f
Brett Wooldridge <brett.wooldridge@gmail.com>
2013-10-31 14:51:12 +0900

    Shell script to run benchmark.

---
+++ benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CLASSPATH=$CLASSPATH:~/.m2/repository/org/javassist/javassist/3.18.1-GA/javassist-3.18.1-GA.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/org/javassist/javassist/3.18.1-GA/javassist-3.18.1-GA.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/org/slf4j/slf4j-api/1.7.5/slf4j-api-1.7.5.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/org/slf4j/slf4j-simple/1.7.5/slf4j-simple-1.7.5.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/com/jolbox/bonecp/0.8.0-rc3/bonecp-0.8.0-rc3.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/com/google/guava/guava/15.0/guava-15.0.jar
+CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
+CLASSPATH=$CLASSPATH:./target/HikariCP-0.9-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:./target/test-classes
+
+java -classpath $CLASSPATH \
+-server -XX:+UseParallelGC -Xss256k -Xms128m -Xmx256m -Dorg.slf4j.simpleLogger.defaultLogLevel=info com.zaxxer.hikari.performance.Benchmark1 $1 $2 $3
