ccc2989c720238ab4842a338b959a3b3a06f34f2
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-01-14 21:18:40 +0900

    Remove unused import.

--- src/test/java/com/zaxxer/hikari/CreationTest.java
+++ src/test/java/com/zaxxer/hikari/CreationTest.java
@@ -20,7 +20,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
