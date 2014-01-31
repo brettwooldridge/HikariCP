55d7ab2254c0d00192a6136c00f0cc6f78d60b5f
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-01-31 21:28:29 +0100

    Move up to bone release.

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ CLASSPATH=$CLASSPATH:~/.m2/repository/org/javassist/javassist/3.18.1-GA/javassis
 CLASSPATH=$CLASSPATH:~/.m2/repository/org/slf4j/slf4j-api/1.7.5/slf4j-api-1.7.5.jar
 CLASSPATH=$CLASSPATH:~/.m2/repository/org/slf4j/slf4j-simple/1.7.5/slf4j-simple-1.7.5.jar
 
-CLASSPATH=$CLASSPATH:~/.m2/repository/com/jolbox/bonecp/0.8.0-rc3/bonecp-0.8.0-rc3.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/com/jolbox/bonecp/0.8.0.RELEASE/bonecp-0.8.0.RELEASE.jar
 CLASSPATH=$CLASSPATH:~/.m2/repository/com/google/guava/guava/15.0/guava-15.0.jar
 
 CLASSPATH=$CLASSPATH:~/.m2/repository/org/apache/tomcat/tomcat-jdbc/7.0.47/tomcat-jdbc-7.0.47.jar
@@ -15,4 +15,4 @@ CLASSPATH=$CLASSPATH:./target/HikariCP-1.2.9-SNAPSHOT.jar
 CLASSPATH=$CLASSPATH:./target/test-classes
 
 java -classpath $CLASSPATH \
--server -XX:+UseParallelGC -Xss256k -Xms128m -Xmx256m -Dorg.slf4j.simpleLogger.defaultLogLevel=info com.zaxxer.hikari.performance.Benchmark1 $1 $2 $3
+-client -XX:MaxInlineSize=0 -XX:+UseParallelGC -Xss256k -Xms128m -Xmx256m -Dorg.slf4j.simpleLogger.defaultLogLevel=info com.zaxxer.hikari.performance.Benchmark1 $1 $2 $3
