575a9fbe705a775e71118f5800a90154c2c77ed8
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-02-07 15:04:00 +0900

    Add class paths for other pools.

--- benchmark.sh
+++ benchmark.sh
@@ -10,6 +10,13 @@ CLASSPATH=$CLASSPATH:~/.m2/repository/com/google/guava/guava/15.0/guava-15.0.jar
 CLASSPATH=$CLASSPATH:~/.m2/repository/org/apache/tomcat/tomcat-jdbc/7.0.47/tomcat-jdbc-7.0.47.jar
 CLASSPATH=$CLASSPATH:~/.m2/repository/org/apache/tomcat/tomcat-juli/7.0.47/tomcat-juli-7.0.47.jar
 
+CLASSPATH=$CLASSPATH:~/.m2/repository/com/mchange/c3p0/0.9.5-pre5/c3p0-0.9.5-pre5.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/com/mchange/mchange-commons-java/0.2.6.2/mchange-commons-java-0.2.6.2.jar
+
+CLASSPATH=$CLASSPATH:~/.m2/repository/net/snaq/dbpool/5.1/dbpool-5.1.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/commons-logging/commons-logging-api/1.1/commons-logging-api-1.1.jar
+
 CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
 CLASSPATH=$CLASSPATH:./target/HikariCP-1.2.9-SNAPSHOT.jar
 CLASSPATH=$CLASSPATH:./target/test-classes
