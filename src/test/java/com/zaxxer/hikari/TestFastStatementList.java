32eadc1d8ccd88aed568e1eaead5d50b04473012
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-03-01 20:11:03 +0900

    Improved FastStatementList

--- src/test/java/com/zaxxer/hikari/TestFastStatementList.java
+++ src/test/java/com/zaxxer/hikari/TestFastStatementList.java
@@ -1,5 +1,8 @@
 package com.zaxxer.hikari;
 
+import java.sql.Statement;
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,15 +14,20 @@ public class TestFastStatementList
     @Test
     public void testOverflow()
     {
+        ArrayList<Statement> verifyList = new ArrayList<>();
+
         FastStatementList list = new FastStatementList();
         for (int i = 0; i < 100; i++)
         {
-            list.add(new StubStatement());
+            StubStatement statement = new StubStatement();
+            list.add(statement);
+            verifyList.add(statement);
         }
 
         for (int i = 0; i < 100; i++)
         {
             Assert.assertNotNull("Element " + i, list.get(i));
+            Assert.assertSame(verifyList.get(i), list.get(i));
         }
     }
 }
