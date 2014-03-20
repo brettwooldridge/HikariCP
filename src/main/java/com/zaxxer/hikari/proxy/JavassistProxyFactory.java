91e4729b8874a876edf0b8704c671d520625dd9f
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-03-21 00:01:21 +0900

    Revert "Use our cascading ClassLoader."
    This reverts commit 183d036173459ed8a58cefda8df0d2465f59ecbe.

--- src/main/java/com/zaxxer/hikari/proxy/JavassistProxyFactory.java
+++ src/main/java/com/zaxxer/hikari/proxy/JavassistProxyFactory.java
@@ -73,7 +73,7 @@ public final class JavassistProxyFactory
     {
         classPool = new ClassPool();
         classPool.importPackage("java.sql");
-        classPool.appendClassPath(new LoaderClassPath(ClassLoaderUtils.getClassLoader()));
+        classPool.appendClassPath(new LoaderClassPath(this.getClass().getClassLoader()));
 
         try
         {
