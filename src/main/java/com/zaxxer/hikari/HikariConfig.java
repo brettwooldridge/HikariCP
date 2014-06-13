70c0e3d96ba8a14efb61e375915027b7346ba49f
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-06-13 17:50:19 +0900

    Fix issue with ordering of checks.  Bounds should only be checked in validate().

--- src/main/java/com/zaxxer/hikari/HikariConfig.java
+++ src/main/java/com/zaxxer/hikari/HikariConfig.java
@@ -546,7 +546,7 @@ public class HikariConfig implements HikariConfigMBean
    @Override
    public void setMinimumIdle(int minIdle)
    {
-      if (minIdle < 0 || minIdle > maxPoolSize) {
+      if (minIdle < 0) {
          throw new IllegalArgumentException("maxPoolSize cannot be negative or greater than maximumPoolSize");
       }
       this.minIdle = minIdle;
@@ -696,7 +696,7 @@ public class HikariConfig implements HikariConfigMBean
          connectionTimeout = CONNECTION_TIMEOUT;
       }
 
-      if (minIdle < 0) {
+      if (minIdle < 0 || minIdle > maxPoolSize) {
          minIdle = maxPoolSize;
       }
 
