076e90b88859e883519c78899bd21eff7bbc68b7
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-12-15 14:26:16 +0900

    Test unwrap exception for Hibernate

--- hikaricp-common/src/test/java/com/zaxxer/hikari/TestHibernate.java
+++ hikaricp-common/src/test/java/com/zaxxer/hikari/TestHibernate.java
@@ -3,6 +3,7 @@ package com.zaxxer.hikari;
 import java.sql.Connection;
 import java.util.Properties;
 
+import org.hibernate.service.UnknownUnwrapTypeException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,6 +25,14 @@ public class TestHibernate
 
       Assert.assertNotNull(provider.unwrap(HikariConnectionProvider.class));
       Assert.assertFalse(provider.supportsAggressiveRelease());
+
+      try {
+         provider.unwrap(TestHibernate.class);
+         Assert.fail("Expected exception");
+      }
+      catch (UnknownUnwrapTypeException e) {
+      }
+
       provider.stop();
    }
 }
