ef43827a7b481bc19b5d738a4c94b62294293eea
Brett Wooldridge <brett.wooldridge@gmail.com>
2015-04-20 22:35:18 +0900

    Fix mis-merge.

--- src/main/java/com/zaxxer/hikari/pool/HikariPool.java
+++ src/main/java/com/zaxxer/hikari/pool/HikariPool.java
@@ -258,7 +258,7 @@ public class HikariPool implements HikariPoolMBean, IBagStateListener
                softEvictConnections();
                abortActiveConnections(assassinExecutor);
             }
-            while (getTotalConnections() > 0 && elapsedTimeMs(start) < TimeUnit.SECONDS.toNanos(5));
+            while (getTotalConnections() > 0 && elapsedTimeMs(start) < TimeUnit.SECONDS.toMillis(5));
          } finally {
             assassinExecutor.shutdown();
             assassinExecutor.awaitTermination(5L, TimeUnit.SECONDS);
