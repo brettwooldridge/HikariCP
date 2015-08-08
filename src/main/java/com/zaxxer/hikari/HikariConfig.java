e789d03571bad8e976877ee9649ec3c9f790d016
Nitin <nitinchauhan@hotmail.com>
2015-08-08 21:07:06 +0530

    Fix Issue 375 ..restore thing that was not broken Fixed Readme ...added missing changes from master

--- src/main/java/com/zaxxer/hikari/HikariConfig.java
+++ src/main/java/com/zaxxer/hikari/HikariConfig.java
@@ -16,13 +16,12 @@
 
 package com.zaxxer.hikari;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
@@ -849,8 +848,8 @@ public class HikariConfig implements HikariConfigMXBean
 
    protected void loadProperties(String propertyFileName)
    {
-      final Path propFile = Paths.get(propertyFileName);
-      try (final InputStream is = Files.isRegularFile(propFile) ? Files.newInputStream(propFile) : HikariConfig.class.getResourceAsStream(propertyFileName)) {
+      final File propFile = new File(propertyFileName);
+      try (final InputStream is = propFile.isFile() ? new FileInputStream(propFile) : this.getClass().getResourceAsStream(propertyFileName)) {
          if (is != null) {
             Properties props = new Properties();
             props.load(is);
