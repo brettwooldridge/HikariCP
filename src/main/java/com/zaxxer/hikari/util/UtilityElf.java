cce7c9133d6cba20ba7f693352c37c31fd8a5067
Brett Wooldridge <brett.wooldridge@gmail.com>
2016-08-14 16:46:51 +0900

    Prepare 2.5.0 release (Java 8 only)

--- src/main/java/com/zaxxer/hikari/util/UtilityElf.java
+++ src/main/java/com/zaxxer/hikari/util/UtilityElf.java
@@ -26,8 +26,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -152,20 +150,6 @@ public final class UtilityElf
       }
 
       return -1;
-   }
-
-   /**
-    * Are we running on JDK 8 or above?
-    *
-    * @return true if JDK 8+, false otherwise
-    */
-   public static boolean isJdk8Plus() {
-      Matcher matcher = Pattern.compile("(?:(\\d+(?:\\.?\\d*)))").matcher(System.getProperty("java.version"));
-      if (!matcher.find()) {
-         return false;
-      }
-
-      return Float.parseFloat(matcher.group(1)) > 1.7f;
    }
 
    public static final class DefaultThreadFactory implements ThreadFactory {
