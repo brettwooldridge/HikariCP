f7fbc841f4b33bf18e175d2a2ca4ad8faa08c54b
Jack <goughy000@gmail.com>
2016-08-17 03:47:25 +0100

    added remove of ConnectionTimeoutRate and refactored metric names into constants (#698)

---
+++ src/test/java/com/zaxxer/hikari/metrics/dropwizard/CodaHaleMetricsTrackerTest.java
@@ -0,0 +1,39 @@
+package com.zaxxer.hikari.metrics.dropwizard;
+
+import com.codahale.metrics.MetricRegistry;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CodaHaleMetricsTrackerTest {
+
+   @Mock
+   public MetricRegistry mockMetricRegistry;
+
+   private CodaHaleMetricsTracker testee;
+
+   @Before
+   public void setup(){
+      testee = new CodaHaleMetricsTracker("mypool", null, mockMetricRegistry);
+   }
+
+   @Test
+   public void close() throws Exception {
+      testee.close();
+
+      verify(mockMetricRegistry).remove("mypool.pool.Wait");
+      verify(mockMetricRegistry).remove("mypool.pool.Usage");
+      verify(mockMetricRegistry).remove("mypool.pool.ConnectionTimeoutRate");
+      verify(mockMetricRegistry).remove("mypool.pool.TotalConnections");
+      verify(mockMetricRegistry).remove("mypool.pool.IdleConnections");
+      verify(mockMetricRegistry).remove("mypool.pool.ActiveConnections");
+      verify(mockMetricRegistry).remove("mypool.pool.PendingConnections");
+   }
+
+}
