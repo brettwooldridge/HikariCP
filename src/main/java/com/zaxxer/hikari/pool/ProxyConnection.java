24e278f35d89455e93a209909a706de22af88b35
Johno Crawford <johno@hellface.com>
2016-09-19 13:34:50 +0200

    Check the correct sql state and error code when unwrapping the exception chain. (#724)

--- src/main/java/com/zaxxer/hikari/pool/ProxyConnection.java
+++ src/main/java/com/zaxxer/hikari/pool/ProxyConnection.java
@@ -151,10 +151,10 @@ public abstract class ProxyConnection implements Connection
       SQLException nse = sqle;
       for (int depth = 0; nse != null && delegate != ClosedConnection.CLOSED_CONNECTION && depth < 10; depth++) {
          final String sqlState = nse.getSQLState();
-         if (sqlState != null && sqlState.startsWith("08") || ERROR_STATES.contains(sqlState) || ERROR_CODES.contains(sqle.getErrorCode())) {
+         if (sqlState != null && sqlState.startsWith("08") || ERROR_STATES.contains(sqlState) || ERROR_CODES.contains(nse.getErrorCode())) {
             // broken connection
             LOGGER.warn("{} - Connection {} marked as broken because of SQLSTATE({}), ErrorCode({})",
-                        poolEntry.getPoolName(), delegate, sqlState, sqle.getErrorCode(), sqle);
+                        poolEntry.getPoolName(), delegate, sqlState, nse.getErrorCode(), nse);
             leakTask.cancel();
             poolEntry.evict("(connection is broken)");
             delegate = ClosedConnection.CLOSED_CONNECTION;
