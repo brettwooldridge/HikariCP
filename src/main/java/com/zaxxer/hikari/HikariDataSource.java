8c7033a3358f31dee6419ac44dfc4141b7c4d862
Brett Wooldridge <brett.wooldridge@gmail.com>
2017-01-26 10:27:59 +0900

    Fixes #742 Add MXBean accessors to HikariDataSource to facilitate implementation of "graceful" shutdown when JMX is not enabled.

--- src/main/java/com/zaxxer/hikari/HikariDataSource.java
+++ src/main/java/com/zaxxer/hikari/HikariDataSource.java
@@ -264,6 +264,28 @@ public class HikariDataSource extends HikariConfig implements DataSource, Closea
    }
 
    /**
+    * Get the {@code HikariPoolMXBean} for this HikariDataSource instance.  If this method is called on
+    * a {@code HikariDataSource} that has been constructed without a {@code HikariConfig} instance,
+    * and before an initial call to {@code #getConnection()}, the return value will be {@code null}.
+    *
+    * @return the {@code HikariPoolMXBean} instance, or {@code null}.
+    */
+   public HikariPoolMXBean getHikariPoolMXBean()
+   {
+      return pool;
+   }
+
+   /**
+    * Get the {@code HikariConfigMXBean} for this HikariDataSource instance.
+    * 
+    * @return the {@code HikariConfigMXBean} instance.
+    */
+   public HikariConfigMXBean getHikariConfigMXBean()
+   {
+      return this;
+   }
+
+   /**
     * Evict a connection from the pool.  If the connection has already been closed (returned to the pool)
     * this may result in a "soft" eviction; the connection will be evicted sometime in the future if it is
     * currently in use.  If the connection has not been closed, the eviction is immediate.
@@ -281,7 +303,11 @@ public class HikariDataSource extends HikariConfig implements DataSource, Closea
    /**
     * Suspend allocation of connections from the pool.  All callers to <code>getConnection()</code>
     * will block indefinitely until <code>resumePool()</code> is called.
+    *
+    * @deprecated Call the {@code HikariPoolMXBean#suspendPool()} method on the {@code HikariPoolMXBean}
+    *             obtained by {@code #getHikariPoolMXBean()} or JMX lookup.
     */
+   @Deprecated
    public void suspendPool()
    {
       HikariPool p;
@@ -292,7 +318,11 @@ public class HikariDataSource extends HikariConfig implements DataSource, Closea
 
    /**
     * Resume allocation of connections from the pool.
+    *
+    * @deprecated Call the {@code HikariPoolMXBean#resumePool()} method on the {@code HikariPoolMXBean}
+    *             obtained by {@code #getHikariPoolMXBean()} or JMX lookup.
     */
+   @Deprecated
    public void resumePool()
    {
       HikariPool p;
