d523cd25a8583ab57585a54c5caaa631b661aba8
Brett Wooldridge <brett.wooldridge@gmail.com>
2017-09-08 03:30:31 +0900

    Fixes #968 Wrong label order in MicrometerMetricsTracker for the connection usage metric.

--- src/main/java/com/zaxxer/hikari/metrics/micrometer/MicrometerMetricsTracker.java
+++ src/main/java/com/zaxxer/hikari/metrics/micrometer/MicrometerMetricsTracker.java
@@ -54,7 +54,7 @@ public class MicrometerMetricsTracker implements IMetricsTracker
 
       this.connectionUsage = meterRegistry
          .summaryBuilder(METRIC_NAME_USAGE)
-         .tags(poolName, METRIC_CATEGORY)
+         .tags(METRIC_CATEGORY, poolName)
          .quantiles(WindowSketchQuantiles.quantiles(0.5, 0.95).create())
          .histogram(buckets(linear(0, 10, 20), TimeUnit.MILLISECONDS))
          .create();
