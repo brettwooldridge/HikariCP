2a9e0185e49e5dfd7cf0757c7cc87c2efa5aa3a5
Brett Wooldridge <brett.wooldridge@gmail.com>
2017-09-22 21:07:53 +0900

    Fix NPE caused by merge.

--- src/main/java/com/zaxxer/hikari/pool/HikariPool.java
+++ src/main/java/com/zaxxer/hikari/pool/HikariPool.java
@@ -272,10 +272,10 @@ public final class HikariPool extends PoolBase implements HikariPoolMXBean, IBag
 
    public void setMetricRegistry(Object metricRegistry)
    {
-      if (metricRegistry.getClass().getName().contains("MetricRegistry")) {
+      if (metricRegistry != null && metricRegistry.getClass().getName().contains("MetricRegistry")) {
          setMetricsTrackerFactory(new CodahaleMetricsTrackerFactory((MetricRegistry) metricRegistry));
       }
-      else if (metricRegistry.getClass().getName().contains("MeterRegistry")) {
+      else if (metricRegistry != null && metricRegistry.getClass().getName().contains("MeterRegistry")) {
          setMetricsTrackerFactory(new MicrometerMetricsTrackerFactory((MeterRegistry) metricRegistry));
       }
       else {
