c509ec1a3f1e19769ee69323972f339cf098ff4b
Enes Açıkoğlu <enes.acikoglu@gmail.com>
2019-04-06 10:56:17 +0300

    #1337 isolation level fix for all values accept string (#1338)
    * #1337 isolation level fix for all values accept string
    
    * #1337 name convension refactor
    
    * #1337 remove unused comment
    
    * review fix for redundant optional

--- src/main/java/com/zaxxer/hikari/util/UtilityElf.java
+++ src/main/java/com/zaxxer/hikari/util/UtilityElf.java
@@ -20,9 +20,8 @@ import static java.lang.Thread.currentThread;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.sql.Connection;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
@@ -165,30 +164,13 @@ public final class UtilityElf
     * @param transactionIsolationName the name of the transaction isolation level
     * @return the int value of the isolation level or -1
     */
-   public static int getTransactionIsolation(final String transactionIsolationName)
-   {
+   public static int getTransactionIsolation(final String transactionIsolationName) {
       if (transactionIsolationName != null) {
          try {
             // use the english locale to avoid the infamous turkish locale bug
-            final String upperName = transactionIsolationName.toUpperCase(Locale.ENGLISH);
-            if (upperName.startsWith("TRANSACTION_")) {
-               Field field = Connection.class.getField(upperName);
-               return field.getInt(null);
-            }
-            final int level = Integer.parseInt(transactionIsolationName);
-            switch (level) {
-               case Connection.TRANSACTION_READ_UNCOMMITTED:
-               case Connection.TRANSACTION_READ_COMMITTED:
-               case Connection.TRANSACTION_REPEATABLE_READ:
-               case Connection.TRANSACTION_SERIALIZABLE:
-               case Connection.TRANSACTION_NONE:
-               case SQL_SERVER_SNAPSHOT_ISOLATION_LEVEL: // a specific isolation level for SQL server only
-                  return level;
-               default:
-                  throw new IllegalArgumentException();
-             }
-         }
-         catch (Exception e) {
+            final String upperCaseIsolationLevelName = transactionIsolationName.toUpperCase(Locale.ENGLISH);
+            return IsolationLevel.valueOf(upperCaseIsolationLevelName).getLevelId();
+         } catch (Exception e) {
             throw new IllegalArgumentException("Invalid transaction isolation value: " + transactionIsolationName);
          }
       }
