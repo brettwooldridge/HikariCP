1640a99d096d40e4a4637e614bf99efad8ea626d
kollstrom <e.kollstrom@gmail.com>
2019-10-29 13:20:28 +0100

    Add name to IsolationLevel enum

--- src/main/java/com/zaxxer/hikari/util/IsolationLevel.java
+++ src/main/java/com/zaxxer/hikari/util/IsolationLevel.java
@@ -16,22 +16,28 @@
 
 package com.zaxxer.hikari.util;
 
-public enum IsolationLevel
-{
-   TRANSACTION_NONE(0),
-   TRANSACTION_READ_UNCOMMITTED(1),
-   TRANSACTION_READ_COMMITTED(2),
-   TRANSACTION_REPEATABLE_READ(4),
-   TRANSACTION_SERIALIZABLE(8),
-   TRANSACTION_SQL_SERVER_SNAPSHOT_ISOLATION_LEVEL(4096);
+public enum IsolationLevel {
+   TRANSACTION_NONE(0, "TRANSACTION_NONE"),
+   TRANSACTION_READ_UNCOMMITTED(1, "TRANSACTION_READ_UNCOMMITTED"),
+   TRANSACTION_READ_COMMITTED(2, "TRANSACTION_READ_COMMITTED"),
+   TRANSACTION_REPEATABLE_READ(4, "TRANSACTION_REPEATABLE_READ"),
+   TRANSACTION_SERIALIZABLE(8, "TRANSACTION_SERIALIZABLE"),
+   TRANSACTION_SQL_SERVER_SNAPSHOT_ISOLATION_LEVEL(4096,
+      "TRANSACTION_SQL_SERVER_SNAPSHOT_ISOLATION_LEVEL");
 
    private final int levelId;
+   private final String levelName;
 
-   IsolationLevel(int levelId) {
+   IsolationLevel(int levelId, String levelName) {
       this.levelId = levelId;
+      this.levelName = levelName;
    }
 
    public int getLevelId() {
       return levelId;
+   }
+
+   public String getLevelName() {
+      return levelName;
    }
 }
