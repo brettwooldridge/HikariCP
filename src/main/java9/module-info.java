aaf4d3a59e62e8312db97d3314ff08f84f199d63
Gili Tzabari <cowwoc2020@gmail.com>
2020-04-30 17:02:38 -0400

    Added module-info.java for Java9

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,11 @@
+module com.zaxxer.hikari
+{
+   exports com.zaxxer.hikari;
+   exports com.zaxxer.hikari.hibernate;
+   exports com.zaxxer.hikari.metrics;
+   exports com.zaxxer.hikari.metrics.dropwizard;
+   exports com.zaxxer.hikari.metrics.micrometer;
+   exports com.zaxxer.hikari.metrics.prometheus;
+   exports com.zaxxer.hikari.pool;
+   exports com.zaxxer.hikari.util;
+}
