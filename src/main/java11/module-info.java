1d05b0ec6775ed58cae7c64bc1d0f6f76f2161c1
Gili Tzabari <cowwoc2020@gmail.com>
2020-05-08 00:24:45 -0400

    Added missing dependencies (#1577)
    * Added missing dependencies.
    
    * Ant needs to be told which dependencies go on the classpath and modulepath.
    
    * Removed unused classpath entry.

--- src/main/java11/module-info.java
+++ src/main/java11/module-info.java
@@ -1,5 +1,16 @@
 module com.zaxxer.hikari
 {
+   requires hibernate.core;
+   requires java.sql;
+   requires java.management;
+   requires java.naming;
+   requires javassist;
+   requires simpleclient;
+   requires slf4j.api;
+   requires metrics.core;
+   requires metrics.healthchecks;
+   requires micrometer.core;
+
    exports com.zaxxer.hikari;
    exports com.zaxxer.hikari.hibernate;
    exports com.zaxxer.hikari.metrics;
