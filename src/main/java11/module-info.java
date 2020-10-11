0c8ac85e015a10e14ff7861ce80e617c7e3fead5
Gili Tzabari <cowwoc2020@gmail.com>
2020-10-11 12:13:36 -0400

    Fixed optional dependencies that were marked as required in module-info.java

--- src/main/java11/module-info.java
+++ src/main/java11/module-info.java
@@ -1,15 +1,15 @@
 module com.zaxxer.hikari
 {
-   requires hibernate.core;
+   requires static hibernate.core;
    requires java.sql;
    requires java.management;
    requires java.naming;
-   requires javassist;
-   requires simpleclient;
+   requires static javassist;
+   requires static simpleclient;
    requires slf4j.api;
-   requires metrics.core;
-   requires metrics.healthchecks;
-   requires micrometer.core;
+   requires static metrics.core;
+   requires static metrics.healthchecks;
+   requires static micrometer.core;
 
    exports com.zaxxer.hikari;
    exports com.zaxxer.hikari.hibernate;
