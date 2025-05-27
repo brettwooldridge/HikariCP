bcf82649be0eddd1dca7797149056cca9e21f7f7
Brett Wooldridge <brett.wooldridge@gmail.com>
2025-05-28 02:05:43 +0900

    attempt to fix javadoc generation issue

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn release:prepare -Prelease && mvn deploy -DperformRelease=true -DskipTests -Dmaven.test.skip=true
+mvn clean && mvn package -DskipTests -Dmaven.test.skip=true && mvn release:prepare -Prelease && mvn deploy -DperformRelease=true -DskipTests -Dmaven.test.skip=true
